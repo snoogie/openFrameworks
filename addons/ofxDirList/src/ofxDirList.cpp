f6baa9fdf2827397bf96fe352c1aa8ad85da748e
arturo <arturo@openframeworks.cc>
2010-02-11 21:10:32 +0100

    added sort method

--- addons/ofxDirList/src/ofxDirList.cpp
+++ addons/ofxDirList/src/ofxDirList.cpp
@@ -143,3 +143,19 @@ int ofxDirList::listDir(string directory){
 	ofLog(OF_LOG_VERBOSE, "ofxDirList - listed %i files in %s", nameArray.size(), directory.c_str());
 	return nameArray.size();
 }
+
+static bool stringCompare( const string &left, const string &right ){
+   for( string::const_iterator lit = left.begin(), rit = right.begin(); lit != left.end() && rit != right.end(); ++lit, ++rit )
+      if( tolower( *lit ) < tolower( *rit ) )
+         return true;
+      else if( tolower( *lit ) > tolower( *rit ) )
+         return false;
+   if( left.size() < right.size() )
+      return true;
+   return false;
+}
+
+void ofxDirList::sort(){
+	std::sort(nameArray.begin(),nameArray.end(),&stringCompare);
+	std::sort(pathArray.begin(),pathArray.end(),&stringCompare);
+}
